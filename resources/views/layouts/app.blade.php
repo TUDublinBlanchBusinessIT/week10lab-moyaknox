62644d1332684aaf1225b5687869736342faeb2d
B00153832 Moya Knox <B00153832@mytudublin.ie>
2025-03-31 15:26:11 +0100

    calander displaying correctly

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
 
         <!-- Bootstrap 5 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
+          <link rel="stylesheet" href="{{asset('css/app.css')}}">
+          <script src="{{asset('js/app.js')}}"></script>
+        </script>
     </head>
 
     <body>
